dfdc12db5299b000036e1b561104108ed45427e2
aba <amadouguedadoba@gmail.com>
2022-11-28 10:33:30 +0100

    checkbox button fixed

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function render() {
       Hello test function <span>{n % 2 ? numberFormat(n) : null}</span>
     </h1>
   );
-  ReactDOM.render(title, document.querySelector("#app2"));
+  //ReactDOM.render(title, document.querySelector("#app2"));
 }
 
 render();
@@ -48,15 +48,17 @@ class Clock extends React.Component {
   constructor(props) {
     super(props);
     this.state = { date: new Date() };
-    this.timer = null;
+    this.timerID = null;
   }
 
-  ComponentDidMount() {
-    this.timer = Window.setInterval(this.tick(), 1000);
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
   }
+  //another classic way to do it !
+  //this.timer = setInterval(this.tick, 1000);
 
   ComponentWillUnMount() {
-    window.clearInterval(this.timer);
+    clearInterval(() => this.timerID(), 1000);
   }
   tick() {
     this.setState({ date: new Date() });
@@ -75,37 +77,89 @@ class Clock extends React.Component {
 class Incrementer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { n: props.start };
-    this.timer = null;
+    this.state = { n: props.start, timerID: null };
+    //this.timeID= null;
   }
-  ComponentDidMount() {
-    window.setInterval(this.increment.bind(this), 1000);
+  componentDidMount() {
+    this.play();
   }
+
   ComponentWillUnMount() {
-    window.clearInterval(this.timer);
+    this.pause();
   }
   increment() {
     this.setState((state, props) => ({ n: state.n + props.step }));
   }
+  pause() {
+    clearInterval(this.state.timerID);
+    this.setState({
+      timerID: null,
+    });
+  }
 
+  play() {
+    this.setState({
+      timerID: setInterval(this.increment.bind(this), 1000),
+    });
+  }
+  toggle() {
+    return this.state.timerID ? this.pause() : this.play();
+  }
+  label() {
+    return this.state.timerID ? "Pause" : "Play";
+  }
+  reset() {
+    this.pause();
+    this.play();
+    this.setState((state, props) => ({ n: props.start }));
+  }
   render() {
-    return <div>Valeur : {this.state.n}</div>;
+    return (
+      <div className="form-group">
+        Valeur : {this.state.n}
+        <button
+          className="alert alert-success"
+          onClick={this.toggle.bind(this)}
+        >
+          {this.label()}
+        </button>
+        <button className="alert alert-danger" onClick={this.reset.bind(this)}>
+          Reset
+        </button>
+      </div>
+    );
   }
 }
-
+//method 1
+//this.state.timerID ? <button onClick={this.pause.bind(this)} >Pause</button> :
+//<button onClick={this.play.bind(this)}> Play</button>
 Incrementer.defaultProps = {
   start: 0,
   step: 1,
 };
 
+/**class ManualIncrementor extends React.Component {
+ constructor(props) {
+   super(props)
+   this.state = {n : 0}  
+ }
+ increment (e){
+  e.preventDefault()
+   this.setState((state, props) => ({n: state.n + 1}) );
+ }
+ 
+ render () {
+   return <div>Valeur : {this.state.n} <button onClick={this.increment.bind(this)}>Increment</button></div>
+ }
+}*/
 function Home() {
   return (
     <div>
       <Welcome name="PICA" />
       <Welcome name="ZORO" />
       <Clock />
-      <Incrementer start={10} />
-      <Incrementer start={20} step={2} />
+
+      <Incrementer />
     </div>
   );
 }
@@ -116,5 +170,69 @@ function Home() {
 );*/
 const root = ReactDOM.createRoot(document.querySelector("#app"));
 root.render(<Home name="douam" />);
-
 //ReactDOM.render(<Clock />, document.querySelector("#app2"));
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      firstName: "",
+      newsletter: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const name = e.target.name;
+    const type = e.target.type;
+    const value = type === "checkbox" ? e.target.checked : e.target.value;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="form-group">
+          <label htmlFor="firstName">Your firstName </label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            className="form-control"
+            value={this.state.firstName}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="name">Your Second Name </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="form-control"
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="newsletter">Subscribe to Newsletter</label>
+          <input
+            type="checkbox"
+            name="newsletter"
+            id="newsletter"
+            className="form-control"
+            checked={this.state.newsletter}
+            onChange={this.handleChange}
+          />
+          {JSON.stringify(this.state)}
+        </div>
+      </div>
+    );
+  }
+}
+const root2 = ReactDOM.createRoot(document.querySelector("#app2"));
+root2.render(<App />);
