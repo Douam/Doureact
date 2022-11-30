041715336dd62e974a96db6b1ed3d583ae20158c
aba <amadouguedadoba@gmail.com>
2022-11-30 15:46:11 +0100

    feat bug fixes productlist

--- app.js
+++ app.js
@@ -31,14 +31,50 @@ const PRODUCTS = [
   },
   { category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7" },
 ];
+function ProductRow({ product }) {
+  return (
+    <tr>
+      <td>{product.name}</td>
+      <td>{product.price}</td>
+    </tr>
+  );
+}
+function ProductCategoryRow({ category }) {
+  return (
+    <tr>
+      <th colSpan="2">{category}</th>
+    </tr>
+  );
+}
 
+function ProductTable({ products }) {
+  const rows = [];
+  let lastCategory = null;
+  products.forEach((product) => {
+    if (product.category !== lastCategory) {
+      lastCategory = product.category;
+      rows.push(<ProductCategoryRow category={product.category} />);
+    }
+    rows.push(<ProductRow product={product} />);
+  });
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
 class FilterableTableProduct extends React.Component {
   render() {
     const { products } = this.props;
-    return <div>{JSON.stringify(products)}</div>;
+    return <ProductTable products={products} />;
   }
 }
 const root2 = ReactDOM.createRoot(document.querySelector("#app2"));
-root2.render(<App />);
-(<FilterableTableProduct products={PRODUCTS} />),
+root2.render(<FilterableTableProduct products={PRODUCTS} />),
   document.querySelector("#app");
